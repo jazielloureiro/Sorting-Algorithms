576c963dbf7c2bd08eb72d33d6caaf3ea86fd542
jazielloureiro <jazielloureiro17@gmail.com>
2020-10-27 09:00:19 -0300

    Add shell_sort.c

---
+++ src/shell_sort.c
@@ -0,0 +1,41 @@
+#include "array.h"
+
+void insertion_sortH(int *array, int h){
+	int i;
+	
+	for(i = h; i < SIZE_ARRAY; i += h){
+		int j;
+		
+		for(j = i; j - h >= 0; j -= h){
+			if(array[j] < array[j - h])
+				swap(&array[j], &array[j - h]);
+			else
+				break;
+		}
+	}
+}
+
+void shell_sort(int *array){
+	int h;
+
+	for(h = 1; h < SIZE_ARRAY; h = 3 * h - 1);
+
+	do{
+		h /= 3;
+		insertion_sortH(array, h);
+	}while(h != 1);
+}
+
+int main(){
+	int array[SIZE_ARRAY];
+	
+	fill_array(array);
+	
+	print_array("Unsorted array", array);
+	
+	shell_sort(array);
+	
+	print_array("Sorted array", array);
+
+	return 0;
+}
