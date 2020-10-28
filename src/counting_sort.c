8985dead624e7f4b5ba98247ccd5977a590cca82
jazielloureiro <jazielloureiro17@gmail.com>
2020-10-28 10:24:15 -0300

    Add counting_sort.c

---
+++ src/counting_sort.c
@@ -0,0 +1,38 @@
+#include "array.h"
+
+void counting_sort(int *array){
+	int max, i, j;
+	
+	for(i = 1, max = array[0]; i < SIZE_ARRAY; i++)
+		if(max < array[i])
+			max = array[i];
+			
+	int array_aux[max + 1];
+	
+	for(i = 0; i <= max; i++)
+		array_aux[i] = 0;
+		
+	for(i = 0; i < SIZE_ARRAY; i++)
+		array_aux[array[i]]++;
+		
+	for(i = 0, j = 0; i <= max; i++){
+		while(array_aux[i] != 0){
+			array[j++] = i;
+			array_aux[i]--;
+		}
+	}
+}
+
+int main(){
+	int array[SIZE_ARRAY];
+	
+	fill_array(array);
+	
+	print_array("Unsorted array", array);
+	
+	counting_sort(array);
+	
+	print_array("Sorted array", array);
+
+	return 0;
+}
